b77f021075e54413439044587fe83de4a248d0b8
jack <jackcheah2@gmail.com>
2021-12-21 13:39:10 +0800

    Finished Day 1

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,1 +1,18 @@
 module Day1 where
+
+import Prelude
+
+main :: IO ()
+main = do
+  input <- readFile "input/day1.txt"
+
+  let inputs = read <$> lines input
+  putStrLn "Answer:"
+  print $ counting inputs
+
+counting :: [Int] -> Int
+counting xs = go xs 0 0
+  where
+    go [] _ count = count
+    go (x : xs) 0 count = go xs x count
+    go (x : xs) acc count = if x > acc then go xs x count + 1 else go xs x count
