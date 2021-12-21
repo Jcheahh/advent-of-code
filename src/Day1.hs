1c69b4bb2fc4b0b63d8c18c6274f9836013cfbfd
jack <jackcheah2@gmail.com>
2021-12-21 18:59:24 +0800

    Finished Day 1 part 2

--- src/Day1.hs
+++ src/Day1.hs
@@ -7,12 +7,46 @@ main = do
   input <- readFile "input/day1.txt"
 
   let inputs = read <$> lines input
-  putStrLn "Answer:"
-  print $ counting inputs
+  putStrLn "Part 1:"
+  print $ part1 inputs
+  putStrLn "Part 2:"
+  print $ part2 inputs
 
-counting :: [Int] -> Int
-counting xs = go xs 0 0
+part1 :: [Int] -> Int
+part1 xs = go xs 0 0
   where
     go [] _ count = count
     go (x : xs) 0 count = go xs x count
-    go (x : xs) acc count = if x > acc then go xs x count + 1 else go xs x count
+    go (x : xs) acc count = if x > acc then go xs x (count + 1) else go xs x count
+
+part1' :: [Int] -> Int
+part1' xs =
+  length
+    . filter id
+    . zipWith (>) (tail xs)
+    $ xs
+
+part2 :: [Int] -> Int
+part2 xs = go xs 0 0
+  where
+    go (x : y : z : xs) 0 count = go (y : z : xs) (x + y + z) count
+    go (x : y : z : xs) acc count =
+      if x + y + z > acc
+        then go (y : z : xs) (x + y + z) (count + 1)
+        else go (y : z : xs) (x + y + z) count
+    go _ _ count = count
+
+part2' :: [Int] -> Int
+part2' xs = go xs 0
+  where
+    go (a : b : c : d : xs) count =
+      if d > a
+        then go (b : c : d : xs) $ count + 1
+        else go (b : c : d : xs) count
+    go _ count = count
+
+part2'' :: [Int] -> Int
+part2'' (x : y : xs) =
+  part1 $
+    zipWith3 (\a b c -> a + b + c) (x : y : xs) (y : xs) xs
+part2'' _ = 0
