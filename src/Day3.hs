b07237bbe4db45030485341a9b07909ca7c2b3b5
jack <jackcheah2@gmail.com>
2021-12-27 15:05:49 +0800

    Finshed Day 3 part 2

--- src/Day3.hs
+++ src/Day3.hs
@@ -9,16 +9,24 @@ main = do
       inputs' = transpose inputs
   putStrLn "Part 1:"
   print $ powerConsumption inputs'
+  putStrLn "Part 2:"
+  print $ powerConsumption inputs'
 
---   print $ powerConsumption inputs'
+  case parseBits $ lines input of
+    Nothing -> error "error"
+    Just xs -> do
+      putStrLn "Testing123"
+      print $ oxygen xs * cO2 xs
+      putStrLn "Testing123"
+      print $ cO2 xs
 
--- bintodec :: Integral i => i -> Maybe i
--- bintodec 0 = Just 0
--- bintodec i
---   | last < 2 = fmap (\x -> 2 * x + last) (bintodec (div i 10))
---   | otherwise = Nothing
---   where
---     last = mod i 10
+parseBits :: [String] -> Maybe [[Bit]]
+parseBits xs = sequence . sequence $ (mapM . mapM) parseBit xs
+
+parseBit :: Char -> Maybe Bit
+parseBit '0' = Just Zero
+parseBit '1' = Just One
+parseBit _ = Nothing
 
 binToDec :: [Bit] -> Int
 binToDec xs = sum $ zipWith go (reverse xs) [0 ..]
@@ -26,10 +34,7 @@ binToDec xs = sum $ zipWith go (reverse xs) [0 ..]
     go Zero _ = 0
     go One i = 2 ^ i
 
-data Bit = Zero | One deriving (Eq, Ord)
-
--- mostCommon :: [Bit] -> Bit
--- mostCommon xs = snd $ maximum [([1 | y <- xs, y == x], x) | x <- xs]
+data Bit = Zero | One deriving (Eq, Ord, Show)
 
 mostCommon :: [Bit] -> Bit
 mostCommon xs = go xs (0, 0)
@@ -58,3 +63,39 @@ powerConsumption xs =
       gamma = binToDec ys
       epsilon = binToDec $ fmap notBit ys
    in gamma * epsilon
+
+zeroOneCountHead :: [[Bit]] -> (Int, Int)
+zeroOneCountHead xs = go xs (0, 0)
+  where
+    go [] x = x
+    go (x : xs) (a, b) = case head x of
+      Zero -> go xs (a + 1, b)
+      One -> go xs (a, b + 1)
+
+mostCommonHead :: [[Bit]] -> Bit
+mostCommonHead xs =
+  let (a, b) = zeroOneCountHead xs
+   in if a > b then Zero else One
+
+leastCommonHead :: [[Bit]] -> Bit
+leastCommonHead xs
+  | a == 0 && b == 0 = error ":)"
+  | a == 0 = One
+  | b == 0 = Zero
+  | otherwise = if a > b then One else Zero
+  where
+    (a, b) = zeroOneCountHead xs
+
+oxygen :: [[Bit]] -> Int
+oxygen = generalFunction mostCommonHead
+
+cO2 :: [[Bit]] -> Int
+cO2 = generalFunction leastCommonHead
+
+generalFunction :: ([[Bit]] -> Bit) -> [[Bit]] -> Int
+generalFunction f xs = binToDec $ go xs []
+  where
+    go xs@((_ : _) : _) acc =
+      let b = f xs
+       in go (fmap tail $ filter (\x -> head x == b) xs) (b : acc)
+    go _ acc = reverse acc
