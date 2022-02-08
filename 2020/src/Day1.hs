1bb2eb61adc5d09a5001d308f6791193a5faa515
jack <jackcheah2@gmail.com>
2022-02-08 12:03:08 +0800

    Finished Day 1 part 1 2020

--- 2020/src/Day1.hs
+++ 2020/src/Day1.hs
@@ -1,1 +1,13 @@
-module Day1 where
+module Day1 (main) where
+
+main :: IO ()
+main = do
+  input <- readFile "2020/input/day1.txt"
+  let inputs = read <$> lines input :: [Int]
+  print $ part1 inputs
+
+part1 :: [Int] -> Int
+part1 xs = go xs []
+  where
+    sumOf2020 x = 2020 - x
+    go (x : xs) map = if elem (sumOf2020 x) map then (sumOf2020 x) * x else go xs (x : map)
