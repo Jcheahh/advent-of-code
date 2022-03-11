0e40329a103a1844ed14ee51557dbb0b32adbe87
jack <jackcheah2@gmail.com>
2022-03-11 17:55:04 +0800

    Finished Day 8 part 2 2020

--- year2020/src/Day8.hs
+++ year2020/src/Day8.hs
@@ -7,10 +7,9 @@ main = do
   input <- readFile "year2020/input/day8.txt"
   let inputs = (zipIns . parseIns . lines) input
   print "Part 1"
-  -- 613
   print $ part1 inputs
-
--- print inputs
+  print "Part 2"
+  print $ part2 inputs
 
 data Operation = Accumulator | Jumps | NoOperation deriving (Ord, Eq, Show)
 
@@ -57,5 +56,41 @@ part1 xs = go xs 0 0
       where
         (Instruction op signedNumber, bool) = xs !! index
         alterBool = setAt index (\(i, b) -> if b then (i, b) else (i, not b)) xs
+        operator = head signedNumber
+        number = read $ tail signedNumber :: Int
+
+changeInstruc :: [(Instruction, Bool)] -> Int -> [(Instruction, Bool)]
+changeInstruc [] n = []
+changeInstruc (x : xs) n
+  | n < 0 = x : xs
+  | n > 0 = x : changeInstruc xs (n -1)
+  | otherwise = case op of
+    Accumulator -> x : xs
+    Jumps -> (Instruction NoOperation signedNumber, bool) : xs
+    NoOperation -> (Instruction Jumps signedNumber, bool) : xs
+  where
+    (Instruction op signedNumber, bool) = x
+
+part2 :: [(Instruction, Bool)] -> Int
+part2 ys = go ys 0 0 0
+  where
+    go [] acc _ _ = acc
+    go xs acc index indexToChange
+      | index >= length xs = acc
+      | bool || index < 0 = go (changeInstruc ys indexToChange) 0 0 (indexToChange + 1)
+      | otherwise = case op of
+        Accumulator ->
+          if operator == '+'
+            then go alterBool (acc + number) (index + 1) indexToChange
+            else go alterBool (acc - number) (index + 1) indexToChange
+        Jumps ->
+          if operator == '+'
+            then go alterBool acc (index + number) indexToChange
+            else go alterBool acc (index - number) indexToChange
+        NoOperation -> go alterBool acc (index + 1) indexToChange
+      where
+        (Instruction op signedNumber, bool) = xs !! index
+        alterBool =
+          setAt index (\(i, b) -> if b then (i, b) else (i, not b)) xs
         operator = head signedNumber
         number = read $ tail signedNumber :: Int
